c072f6077b916b38b72cb96666e4bb20b02296a6
WLUB <lukas.bergstrom99@gmail.com>
2023-05-31 22:09:37 +0200

    New fetures and smaller changes

--- src/player.asm
+++ src/player.asm
@@ -6,7 +6,7 @@ global _player_render
 global _player_update
 
 ; redball.asm
-extern _redball_spawn
+extern _redball_interact
 
 ; utils.asm
 extern _utils_exit_and_print_sdl_error
@@ -18,21 +18,34 @@ extern _SDL_GetKeyboardState
 extern _IMG_Load
 
 section .data
-    img_path                db "./assets/player.png",0
+    player_img_path         db "./assets/player.png",0
+    player_happy_img_path   db "./assets/player_happy.png",0
     speed                   equ 5
     
 section .bss
     player_surface          resq 1 
+    player_happy_surface    resq 1 
+
     player_rect             resb 32 
-    player_dir              resb 4 
+    player_dir              resb 1 
+    player_have_the_ball    resb 1 
     
 section .text
 
 _player_load:
     ; Load image
-    mov rdi, img_path  ; src
+    mov rdi, player_img_path  ; src
     call _IMG_Load
     mov [rel player_surface], rax
+
+    ; Check for errors
+    test rax, rax                   
+    jz error   
+
+    ; Load image
+    mov rdi, player_happy_img_path  ; src
+    call _IMG_Load
+    mov [rel player_happy_surface], rax
 
     ; Check for errors
     test rax, rax                   
@@ -46,43 +59,71 @@ _player_load:
     mov [rel player_rect + 8 ], rbx  ;   w
     mov [rel player_rect + 12], rbx  ;   h
 
+    ; Player start with the ball
+    mov r8b, 1
+    mov [rel player_have_the_ball], r8b
+
     ret
 
 _player_free:
     ; Free player image
     mov rdi, [rel player_surface] 
     call _SDL_FreeSurface
+    
+    mov rdi, [rel player_happy_surface]
+    call _SDL_FreeSurface
+
     ret
 
 _player_render:
-    mov rdx, rdi                    ; dst (input)
-    mov rdi, [rel player_surface]   ; src
-    mov rsi, 0                      ; rect
-    mov rcx, player_rect            ; dstrect
-    call _SDL_UpperBlit             ; error code?? 0 on success? 
-    
-    ; Check for errors
-    test rax, rax                   
-    jnz error
+    cmp BYTE [rel player_have_the_ball], 1
+    je render_player
+    render_player_happy:
 
+        mov rdx, rdi                        ; dst (input)
+        mov rdi, [rel player_happy_surface] ; src
+        mov rsi, 0                          ; rect
+        mov rcx, player_rect                ; dstrect
+        call _SDL_UpperBlit                 ; error code?? 0 on success? 
+        
+        ; Check for errors
+        test rax, rax                   
+        jnz error
+
+    jmp _player_render_end
+    render_player:
+
+        mov rdx, rdi                    ; dst (input)
+        mov rdi, [rel player_surface]   ; src
+        mov rsi, 0                      ; rect
+        mov rcx, player_rect            ; dstrect
+        call _SDL_UpperBlit             ; error code?? 0 on success? 
+        
+        ; Check for errors
+        test rax, rax                   
+        jnz error
+
+
+    _player_render_end:
     ret
 
 _player_update:
+
     mov r8,  [rel player_rect + 0]
     mov r9,  [rel player_rect + 4]
 
     ; We need a dir to be able to move.
-    mov ax, [rel player_dir]
-    cmp ax, 0
+    mov al, [rel player_dir]
+    cmp al, 0
     je update_end
     
     ; Calculate new positon for x
-    mov ax, [rel player_dir]
-    or  ax, 0b0011
-    xor ax, 0b0011
-    cmp ax, 0b1000
+    mov al, [rel player_dir]
+    or  al, 0b00000011
+    xor al, 0b00000011
+    cmp al, 0b00001000
     je move_left
-    cmp ax, 0b0100
+    cmp al, 0b00000100
     jne x_move_end
     add r8, speed
     jmp x_move_end
@@ -91,12 +132,12 @@ _player_update:
     x_move_end:
 
     ; Calculate new positon for y
-    mov ax, [rel player_dir]
-    or  ax, 0b1100
-    xor ax, 0b1100
-    cmp ax, 0b0010
+    mov al, [rel player_dir]
+    or  al, 0b00001100
+    xor al, 0b00001100
+    cmp al, 0b00000010
     je move_up
-    cmp ax, 0b0001
+    cmp al, 0b00000001
     jne y_move_end
     add r9, speed
     jmp y_move_end
@@ -124,6 +165,9 @@ _player_update:
     mov [rel player_rect + 4], r9
     no_y_move:
     update_end:
+
+    ; Return a pointer to position
+    mov rax, player_rect
     ret
 
 _player_move:
@@ -133,81 +177,59 @@ _player_move:
     mov rdi, 0      ; nullptr
     call _SDL_GetKeyboardState
 
-    ; SDL_SCANCODE_RIGHT (79)
-    add rax, 79  
+    ; SDL_SCANCODE_SPACE (44)
+    ; Throw ball in the same direction as 
+    ; the player.
+    add rax, 44  
     cmp dword [rax], 1
-    jne no_redball_r
-    mov rdi, [rel player_rect + 0]
-    mov rsi, [rel player_rect + 4]
-    mov rdx, 0b0100
-    call _redball_spawn
-    no_redball_r:
-    sub rax, 79  
+    jne no_interact
 
-    ; SDL_SCANCODE_LEFT (80)
-    add rax, 80  
-    cmp dword [rax], 1
-    jne no_redball_l
-    mov rdi, [rel player_rect + 0]
-    mov rsi, [rel player_rect + 4]
-    mov rdx, 0b1000
-    call _redball_spawn
-    no_redball_l:
-    sub rax, 80  
-    
-    ; SDL_SCANCODE_DOWN (81)
-    add rax, 81  
-    cmp dword [rax], 1
-    jne no_redball_d
-    mov rdi, [rel player_rect + 0]
-    mov rsi, [rel player_rect + 4]
-    mov rdx, 0b0001
-    call _redball_spawn
-    no_redball_d:
-    sub rax, 81  
+    interact:
 
-    ; SDL_SCANCODE_UP (82)
-    add rax, 82  
-    cmp dword [rax], 1
-    jne no_redball_u
-    mov rdi, [rel player_rect + 0]
-    mov rsi, [rel player_rect + 4]
-    mov rdx, 0b0010
-    call _redball_spawn
-    no_redball_u:
-    sub rax, 82 
+        mov rdi, [rel player_rect + 0]
+        mov rsi, [rel player_rect + 4]
+        mov dl , [rel player_dir] 
+        
+        ; Return true/false if player have the ball
+        call _redball_interact
+        mov [rel player_have_the_ball], al
 
-    mov r8, 0
-    
-    ; SDL_SCANCODE_A (4)
-    add rax, 4  ; 0 + 4  = 4
-    cmp dword [rax], 1
-    jne not_scancode_A
-    or r8, 0b1000
-    not_scancode_A:
+    no_interact:
+    ; Restore pointer
+    sub rax, 44  
 
-    ; SDL_SCANCODE_D (7)
-    add rax, 3  ; 4 + 3  = 7
-    cmp dword [rax], 1
-    jne not_scancode_B
-    or r8, 0b0100
-    not_scancode_B:
+        ; r8b low 8 bit
+        mov r8b, 0
+        
+        ; SDL_SCANCODE_A (4)
+        add rax, 4  ; 0 + 4  = 4
+        cmp dword [rax], 1
+        jne not_scancode_A
+        or r8b, 0b00001000
+        not_scancode_A:
 
-    ; SDL_SCANCODE_S (22)
-    add rax, 15  ; 7 + 15  = 22
-    cmp dword [rax], 1
-    jne not_scancode_S
-    or r8, 0b0001
-    not_scancode_S:
+        ; SDL_SCANCODE_D (7)
+        add rax, 3  ; 4 + 3  = 7
+        cmp dword [rax], 1
+        jne not_scancode_B
+        or r8b, 0b00000100
+        not_scancode_B:
 
-    ; SDL_SCANCODE_W (26)
-    add rax, 4  ; 22 + 4  = 26 
-    cmp dword [rax], 1
-    jne not_scancode_W
-    or r8, 0b0010
-    not_scancode_W:
+        ; SDL_SCANCODE_S (22)
+        add rax, 15  ; 7 + 15  = 22
+        cmp dword [rax], 1
+        jne not_scancode_S
+        or r8b, 0b00000001
+        not_scancode_S:
 
-    mov [rel player_dir], r8d
+        ; SDL_SCANCODE_W (26)
+        add rax, 4  ; 22 + 4  = 26 
+        cmp dword [rax], 1
+        jne not_scancode_W
+        or r8b, 0b00000010
+        not_scancode_W:
+
+        mov [rel player_dir], r8b
 
     ret
 
--- src/redball.asm
+++ src/redball.asm
@@ -1,7 +1,7 @@
 ; redball.asm
 global _redball_load
 global _redball_free
-global _redball_spawn
+global _redball_interact
 global _redball_render
 global _redball_update
 
@@ -19,10 +19,10 @@ section .data
     speed               equ 10
 
 section .bss
-    redball_surface    resq 1 
-    redball_rect       resb 32 
-    redball_dir        resb 4 
-    redball_available  resb 1
+    redball_surface     resq 1 
+    redball_rect        resb 32 
+    redball_dir         resb 1 
+    redball_in_air      resb 1
 
 section .text
 
@@ -38,16 +38,16 @@ _redball_load:
 
     ; Init redball rect
     ; Spawn it outside of the screen
-    mov rax, 1000
-    mov rbx, 32
-    mov [rel redball_rect + 0 ], rax  ;   x
-    mov [rel redball_rect + 4 ], rax  ;   y
-    mov [rel redball_rect + 8 ], rbx  ;   w
-    mov [rel redball_rect + 12], rbx  ;   h
+    mov eax, 1000
+    mov ebx, 32
+    mov [rel redball_rect + 0 ], eax  ;   x
+    mov [rel redball_rect + 4 ], eax  ;   y
+    mov [rel redball_rect + 8 ], ebx  ;   w
+    mov [rel redball_rect + 12], ebx  ;   h
     
     ; Make it available
     mov ch, 1
-    mov [rel redball_available], ch
+    mov [rel redball_in_air], ch
     
     ret
 
@@ -57,23 +57,83 @@ _redball_free:
     call _SDL_FreeSurface
     ret
 
-_redball_spawn:
+; Inputs: x (32 bit) y (32 bit) direction (8 bit) 
+; Output: in air (1/0)
+_redball_interact:
     ; Make sure we can only fire one at a time
     ; We don't have any system in place to spawn 
     ; more then one... 
-    cmp BYTE [rel redball_available], 1
-    jne redball_spawn_end
+    cmp BYTE [rel redball_in_air], 0
+    je redball_catch
+    redball_throw:
+        mov [rel redball_rect + 0 ], edi  ;   x (input)
+        mov [rel redball_rect + 4 ], esi  ;   y (input)
 
-    mov [rel redball_rect + 0 ], edi  ;   x (input)
-    mov [rel redball_rect + 4 ], esi  ;   y (input)
-    mov [rel redball_dir], edx        ;   direction (input)
+        ; If input direction is zero we
+        ; throw the ball down.
+        cmp rdx, 0
+        jne redball_spawn_valid_dir
+        mov dl, 0b00000001          
+        redball_spawn_valid_dir:
+        
+        mov [rel redball_dir], dl   ;   direction (input)
 
-    ; Make the ball unavailable
-    mov bh, 0
-    mov [rel redball_available], bh
+        ; Make the ball unavailable
+        mov bl, 0
+        mov [rel redball_in_air], bl
 
-    redball_spawn_end:
-    ret
+        ; Return false
+        mov al, 0
+
+        jmp _redball_interact_end
+
+    redball_catch:
+        ; edi  ;   x (input)
+        ; esi  ;   y (input)
+
+        ; Is the ball inside [-32, 96] of player.x?
+        mov r8d, edi
+        sub r8d, 32
+        cmp dword [rel redball_rect + 0], r8d 
+        jl no_intersection
+
+        mov r8d, edi
+        add r8d, 96 ; user is 64 we make it larger
+        mov r9d, [rel redball_rect + 0]
+        add r9d, 32
+
+        cmp r9d, r8d 
+        jg no_intersection
+
+        ; Is the ball inside [-20, 84] of player.y?
+        mov r8d, esi
+        sub r8d, 32
+        cmp dword [rel redball_rect + 4], esi 
+        jl no_intersection
+
+        mov r8d, esi
+        add r8d, 96 ; user is 64 we make it larger
+        mov r9d, [rel redball_rect + 4]
+        add r9d, 32 
+
+        cmp r9d, r8d 
+        jg no_intersection
+
+        ; Move ball 
+        mov r8d, 1000
+        mov [rel redball_rect + 0 ], r8d  ;   x
+        mov [rel redball_rect + 4 ], r8d  ;   y
+        
+        ; Make the ball available
+        mov bl, 1
+        mov [rel redball_in_air], bl
+
+        ; Return true
+        mov al, 1
+
+        no_intersection:
+    _redball_interact_end:
+        ret
 
 _redball_render:
     mov rdx, rdi                    ; dst (input)
@@ -88,70 +148,85 @@ _redball_render:
 
     ret
 
-
 _redball_update:
-    mov r8,  [rel redball_rect + 0]
-    mov r9,  [rel redball_rect + 4]
+    ; If the ball issnt in the air we make it
+    ; follow the player.
+    cmp BYTE [rel redball_in_air], 1
+    jne fly_around
+    follow_player:
 
-    ; We need a dir to be able to move.
-    mov ax, [rel redball_dir]
-    cmp ax, 0
-    je update_end
-    
-    ; Calculate new positon for x
-    mov ax, [rel redball_dir]
-    or  ax, 0b0011
-    xor ax, 0b0011
-    cmp ax, 0b1000
-    je move_left
-    cmp ax, 0b0100
-    jne x_move_end
-    add r8, speed
-    jmp x_move_end
-    move_left:
-    sub r8, speed
-    x_move_end:
+        ; Set the ball in the hand of the player
+        mov r8, [rdi]
+        add r8d, 42
+        mov [rel redball_rect + 0], r8d
 
-    ; Calculate new positon for y
-    mov ax, [rel redball_dir]
-    or  ax, 0b1100
-    xor ax, 0b1100
-    cmp ax, 0b0010
-    je move_up
-    cmp ax, 0b0001
-    jne y_move_end
-    add r9, speed
-    jmp y_move_end
-    move_up:
-    sub r9, speed
-    y_move_end:
+        mov r8, [rdi + 4]
+        add r8d, 20
+        mov [rel redball_rect + 4], r8d
 
-    ; Update redball in x-position
-    ; Reference point is top-left corner
-    ; if 64 < x < 640 - 64 * 2
-    cmp r8d, 64   
-    jl destory 
-    cmp r8d, 512   
-    jg destory 
-    mov [rel redball_rect + 0], r8d
-    
-    ; Update redball in x-position
-    ; Reference point is top-left corner
-    ; if 64 < y < 480 - 64 * 2
-    cmp r9d, 64   
-    jl destory 
-    cmp r9d, 352   
-    jg destory 
-    mov [rel redball_rect + 4], r9d
+        jmp update_end
 
-    jmp update_end
+    fly_around:
+        mov r8d,  [rel redball_rect + 0]
+        mov r9d,  [rel redball_rect + 4]
 
-    destory:
-    ; We do not destory it, we move it...
-    mov r8, 1000
-    mov [rel redball_rect + 0], r8d
-    mov ah, 1
-    mov [rel redball_available], ah
+        ; We need a dir to be able to move.
+        mov al, [rel redball_dir]
+        cmp al, 0
+        je update_end
+        
+        ; Calculate new positon for x
+        mov al, [rel redball_dir]
+        or  al, 0b00000011
+        xor al, 0b00000011
+        cmp al, 0b00001000
+        je move_left
+        cmp al, 0b00000100
+        jne x_move_end
+        add r8d, speed
+        jmp x_move_end
+        move_left:
+        sub r8d, speed
+        x_move_end:
+
+        ; Calculate new positon for y
+        mov al, [rel redball_dir]
+        or  al, 0b00001100
+        xor al, 0b00001100
+        cmp al, 0b00000010
+        je move_up
+        cmp al, 0b00000001
+        jne y_move_end
+        add r9, speed
+        jmp y_move_end
+        move_up:
+        sub r9, speed
+        y_move_end:
+
+        ; Update redball in x-position
+        ; Reference point is top-left corner
+        ; if 64 < x < 640 - 64 * 2
+        cmp r8d, 64   
+        jl invert_motion 
+        cmp r8d, 512   
+        jg invert_motion 
+        mov [rel redball_rect + 0], r8d
+        
+        ; Update redball in x-position
+        ; Reference point is top-left corner
+        ; if 64 < y < 480 - 64 * 2
+        cmp r9d, 64   
+        jl invert_motion 
+        cmp r9d, 352   
+        jg invert_motion 
+        mov [rel redball_rect + 4], r9d
+
+        jmp update_end
+        invert_motion:
+
+        mov al, [rel redball_dir]
+        xor al, 0b00001111
+        mov [rel redball_dir], al
 
     update_end:
     ret
